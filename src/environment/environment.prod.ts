4db83388849faf648e9349a78f03b53d906e98c8
Ivarovskiy <ivarovskiy00@gmail.com>
2023-10-18 12:05:13 +0300

    first commit

---
+++ src/environment/environment.prod.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  production: true,
+  apiKey: 'cur_live_XBWBD3a6KbEb4FR5cjamNjpCHS9j4LIA2wi7YtwC',
+  apiSettings: {
+    currencies: 'EUR,USD',
+    base_currency: 'UAH',
+  },
+};
+
+//https://api.freecurrencyapi.com/v1/latest?apikey=fca_live_m4Gwkta09ggK3zPTLZ77xDEzCKR1GVQy63pLOFYi&currencies=EUR%2CUSD%2CCAD
